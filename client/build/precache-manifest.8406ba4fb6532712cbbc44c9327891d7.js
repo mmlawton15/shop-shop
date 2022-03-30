37142ff73dba28f913902063a7d1f83871ac9810
Mary Margaret <mmlawton15@gmail.com>
2022-03-30 10:36:23 -0400

    adding a build folder, missed a step

---
+++ client/build/precache-manifest.8406ba4fb6532712cbbc44c9327891d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7121a423b6b45432a1f582887e3b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2b8c962804be8a358f",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "130c744298700e5891bb",
+    "url": "/static/js/2.03f92a88.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.03f92a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2b8c962804be8a358f",
+    "url": "/static/js/main.c6e296ad.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
